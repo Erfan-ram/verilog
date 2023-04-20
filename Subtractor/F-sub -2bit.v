8e0829b2584beb58256cd68bfd16a9bf4257bac7
Erfan ram <Erfanramezany245@gmail.com>
2023-04-20 21:28:11 +0330

    finalized 2-bit f sub

--- Subtractor/F-sub -2bit.v
+++ Subtractor/F-sub -2bit.v
@@ -17,45 +17,41 @@ and (w2,not_xor_xy,bin);
 or (bout,w2,w1);
 endmodule
 
-module Full_sub_2bit (x,y,bin,diff,bout);
+module Full_sub_2bit (x,y,_bin,diff,_bout);
 
 input [1:0] x,y;
 input _bin;
 output [1:0] diff;
 output _bout;
 
-Full_adder_1bit m0(.x(x[0]),.y(y[0]),.bin(bin),.diff(diff[0]),.bout(w1));
-Full_adder_1bit m1(.x(x[1]),.y(y[1]),.bin(bin),.diff(diff[1]),.bout(_bout));
+Full_sub_1bit m0(.x(x[0]),.y(y[0]),.bin(_bin),.diff(diff[0]),.bout(w1));
+Full_sub_1bit m1(.x(x[1]),.y(y[1]),.bin(w1),.diff(diff[1]),.bout(_bout));
 
 endmodule
 
 
-module test_Full_sub_1bit;
+module test_Full_sub_2bit();
 
-wire _diff,_bout;
-
-reg [1:0]b0,01;
+reg [1:0]b0,b1;
 reg _bin;
 
 wire [1:0]_diff;
 wire _bout;
 
-Full_sub_2bit full(.x(b0),.y(b1),.bin(_bin),.diff(_diff),.bout(_bout));
+Full_sub_2bit full(.x(b0),.y(b1),._bin(_bin),.diff(_diff),._bout(_bout));
 
 
 initial begin
 
-i0=2'b11; i1=2'b11; _bin=1'b0;
+b0=2'b11; b1=2'b11; _bin=1'b0;
 
 #10
 
-i0=2'b11; i1=2'b00; _bin=1'b0;
+b0=2'b11; b1=2'b00; _bin=1'b0;
 
 #10
 
-i0=2'b10; i1=2'b01; _bin=1'b0;
+b0=2'b10; b1=2'b01; _bin=1'b0;
 
-#10
 end
-
 endmodule
